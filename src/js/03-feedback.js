cb3774e07d8dcfc61adec2b50cda5611a0660e3d
Andrey Obibok <andreyobibok@gmail.com>
2022-12-18 18:13:13 +0200

    feedback form fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,13 +4,11 @@ const email = document.querySelector('input[name = "email"]');
 const message = document.querySelector('textarea[name = "message"]');
 const submit = document.querySelector('.feedback-form button');
 
-const formState = JSON.parse(localStorage.getItem("feedback-form-state"));
+const formState = JSON.parse(localStorage.getItem("feedback-form-state")) || {};
 
-try {
-    email.value = formState.email;
-    message.value = formState.message;
-}
-catch (e) {}
+email.value = formState.email || '';
+message.value = formState.message || '';
+
 
 const handleInput = function () {
     let data = {
@@ -23,7 +21,9 @@ const handleInput = function () {
 const handleSubmit = function (evt) {
     evt.preventDefault();
     console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
-    localStorage.clear();
+    localStorage.removeItem("feedback-form-state");
+    email.value = '';
+    message.value = '';
 }
 
 form.addEventListener('input', throttle(handleInput, 500));
